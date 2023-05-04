a242fce03dcd719e6025fe9ab9f7bdc044ad5a36
Ledmington <ledmington.dev@gmail.com>
2023-05-04 16:47:43 +0200

    Added skybox

--- src/main/java/com/ledmington/solarsystem/MainScreen.java
+++ src/main/java/com/ledmington/solarsystem/MainScreen.java
@@ -23,17 +23,21 @@ import java.util.Map;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.Renderable;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.model.NodePart;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
@@ -47,6 +51,7 @@ public final class MainScreen implements Screen {
     // 10'000km : 1
     private static final double scale = 1.0 / 10_000_000.0;
 
+    private final AssetManager assetManager;
     private final PerspectiveCamera camera;
     private final float cameraSpeed = 0.01f;
     private final CameraInputController camController;
@@ -57,8 +62,16 @@ public final class MainScreen implements Screen {
     private final Map<ModelInstance, Body> modelToBody = new HashMap<>();
     private final SpriteBatch spriteBatch = new SpriteBatch();
     private final BitmapFont font = new BitmapFont();
+    private final Environment environment;
+    private boolean loading;
+    private final String skyBoxFileName = "models/skybox.obj";
+    private ModelInstance skyBox;
 
     public MainScreen() {
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
         camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         camera.position.set(2f, 2f, 2f);
         camera.lookAt(0, 0, 0);
@@ -100,6 +113,16 @@ public final class MainScreen implements Screen {
         }
 
         modelBatch = new ModelBatch();
+
+        assetManager = new AssetManager();
+        assetManager.load(skyBoxFileName, Model.class);
+        loading = true;
+    }
+
+    private void doneLoading() {
+        skyBox = new ModelInstance(assetManager.get(skyBoxFileName, Model.class));
+
+        loading = false;
     }
 
     @Override
@@ -109,22 +132,32 @@ public final class MainScreen implements Screen {
 
     @Override
     public void render(final float delta) {
+        if (loading && assetManager.update()) {
+            doneLoading();
+        }
+
         camController.update();
 
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT | GL30.GL_DEPTH_BUFFER_BIT);
 
         modelBatch.begin(camera);
+
+        if (skyBox != null) {
+            skyBox.transform.setToTranslation(camera.position);
+            modelBatch.render(skyBox);
+        }
+
         spriteBatch.begin();
         for (final ModelInstance instance : instances) {
-            modelBatch.render(instance);
+            modelBatch.render(instance, environment);
             if (isVisible(camera, instance)) {
                 final Body b = modelToBody.get(instance);
                 final Vector3 labelPosition = camera.project(new Vector3(
                         (float) ((b.position().x + b.radius()) * scale),
                         (float) ((b.position().y + b.radius()) * scale),
                         (float) (b.position().z * scale)));
-                        
+
                 font.draw(spriteBatch, b.name(), labelPosition.x + 10, labelPosition.y + 10);
             }
         }
